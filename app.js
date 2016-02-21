be7e66b46650d777d5db74f75ce40961c93ad21a
Bryan Dodd <aqualad@gmail.com>
2016-02-20 19:52:16 -0500

    Add init app.js with routing and view

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use('/', routes);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+module.exports = app;
