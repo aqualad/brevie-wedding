34804f77211ff7700d617c4be47287a54c5cf931
Bryan Dodd <aqualad@gmail.com>
2016-02-28 14:28:16 -0500

    Bind params into index view with title attribute

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {title: "Evie McMahon and Bryan Dodd Wedding - September 9th, 2016"});
 });
 
 module.exports = router;
