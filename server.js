2dcdcfd9c8e001b2f3ccf4fb8cda5dce7c3a6319
Bryan Dodd <aqualad@gmail.com>
2016-02-28 22:17:07 -0500

    Refactor structure to use /app

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var path = require('path');
-
-var routes = require('./routes/index');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'hbs');
 
 // Route to static files
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/app', express.static(__dirname + '/app'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-app.use('/', routes);
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
