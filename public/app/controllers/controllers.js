0e8ad8637e217c42eb0c5723420fa5376f0b13e8
Bryan Dodd <aqualad@gmail.com>
2016-03-16 01:50:18 -0400

    Split footer and header into partials

--- public/app/controllers/controllers.js
+++ public/app/controllers/controllers.js
@@ -1,7 +1,20 @@
+"use strict";
+
 var weddingControllers = angular.module('weddingControllers', []);
 
-weddingControllers.controller('HomeController', ['$scope',
-    function ($scope) {
-        $scope.bryan_test = 'Hello';
-    }
-]);
+weddingControllers
+    .controller('HomeController', ['$scope',
+        function ($scope) {
+            $scope.welcome = 'Invite You to Celebrate At their Wedding';
+        }
+    ])
+    .directive('weddingHeader', function() {
+        return {
+            templateUrl: '/app/views/directives/header.hbs'
+        };
+    })
+    .directive('weddingFooter', function() {
+        return {
+            templateUrl: '/app/views/directives/footer.hbs'
+        };
+    });
