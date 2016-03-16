8c9219663fff7a3fbee2a9e545b62f53cf322c05
Bryan Dodd <aqualad@gmail.com>
2016-03-16 00:20:09 -0400

    Use weddingControllers module instead of HomeController

---
+++ public/app/controllers/controllers.js
@@ -0,0 +1,7 @@
+var weddingControllers = angular.module('weddingControllers', []);
+
+weddingControllers.controller('HomeController', ['$scope',
+    function ($scope) {
+        $scope.bryan_test = 'Hello';
+    }
+]);
