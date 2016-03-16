e5b70d37147a2f2dc40ff0e3bcca27c811b1cdd3
Bryan Dodd <aqualad@gmail.com>
2016-03-15 20:10:49 -0400

    Attempt to add angular controllers

--- public/app/app.js
+++ public/app/app.js
@@ -2,4 +2,16 @@
     "use strict";
 
     var weddingApp = angular.module('weddingApp', ['ngRoute']);
+
+    weddingApp.config(['$logProvider', '$routeProvider', function ($logProvider, $routeProvider) {
+
+        $logProvider.debugEnabled(true);
+
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                controllerAs: 'home',
+                templateUrl: '/app/views/index.hbs'
+            });
+    }]);
 }());
---
+++ public/app/controllers/HomeController.js
@@ -0,0 +1,10 @@
+(function () {
+
+    angular.module('weddingApp')
+        .controller('HomeController', [HomeController]);
+
+    function HomeController() {
+        process.exit();
+    }
+
+}());
