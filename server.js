90e728ac26d3a5d53a150c2fbf06e0c0901a164a
Bryan Dodd <aqualad@gmail.com>
2016-03-25 12:59:32 -0400

    Change default app launch announcement

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('WeddingApp listening on port 3000!');
 });
 
 module.exports = app;
