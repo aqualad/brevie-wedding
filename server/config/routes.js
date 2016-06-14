c83c01e46f9166cec2732b90cfb582a7369d4d01
Bryan Dodd <aqualad@gmail.com>
2016-06-14 01:25:25 -0400

    Stubbed TODO for submitting rsvp

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,4 +3,16 @@ module.exports = function(app) {
     app.get('*', function (req, res) {
         res.render('index');
     });
+
+    // Handle RSVP Submission
+    app.post('/rsvp', function (req, res, next) {
+        /* TODO: Implement
+            Validation
+                1. Normal field formatting (phone number, email, etc)
+                2. Check that the phone number, email, or last name exists in the database as a valid guest
+                3. If the guest has already RSVP'd, alert the user
+        */
+
+        res.send({success:true});
+    });
 }
